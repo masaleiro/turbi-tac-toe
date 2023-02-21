dcdb3d1f1587bdcbeee1b8581ac4ddb949377f5e
masaleiro <mariosaleiro@gmail.com>
2023-02-21 23:06:19 +0000

    added players, input and input validation function

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 #/usr/bin/python3
 
+def switchPlayer(players, current_player):
+    if current_player == players[0]:
+        current_player = players[1]
+    else:
+        current_player = players[0]
+
+    return current_player
+
+
+def notValid(board, play):
+    if play in range(1,10) and board[play] in range(1,10):
+        return False
+    else:
+        return True
+
+
+def player_play(board, player):
+    play=-1
+    while notValid(board, play):
+        play = int(input("Player " + player + ", choose position to play from 1 to 9 "))
+    board[play] = player
+
+
 def printBoard(board):
     for i in range(3):
         for j in range(1,4):
-            print(board[i*3+j]+" ",end="")
+            print(str(board[i*3+j])+" ",end="")
         print("\n")
-    pass
+
 
 if __name__ == "__main__":
     print("Welcome to TurbiTacToe")
@@ -15,7 +38,11 @@ if __name__ == "__main__":
     current_state = "MENU"
 
     # create game board
-    board = dict.fromkeys(range(1, 10),"-")
+    board = dict(zip(range(1, 10), range(1, 10)))
+    
+    # create player symbols
+    players = ['X', 'O']
+    current_player = players[0]
 
 
     while(True):
@@ -29,8 +56,9 @@ if __name__ == "__main__":
 
         elif current_state == "GAME":
             printBoard(board)
-            input()
-            pass
+            player_play(board, current_player)
+            
+            current_player = switchPlayer(players, current_player)
 
         elif current_state == "GAMEOVER":
             pass
