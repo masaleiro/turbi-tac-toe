e54d186f576c489a3c202be0946fee1ad61beca7
masaleiro <mariosaleiro@gmail.com>
2023-02-21 22:23:39 +0000

    added application game machine

--- main.py
+++ main.py
@@ -2,3 +2,26 @@
 
 if __name__ == "__main__":
     print("Welcome to TurbiTacToe")
+
+    # setup section
+    previous_state = "SETUP"
+    current_state = "MENU"
+
+    while(True):
+
+        if current_state == "MENU":
+            option = input("Press S to start playing. Any other key to exit.")
+            if option == "S" or option == "s":
+                current_state = "GAME"
+            else:
+                exit()
+
+        elif current_state == "GAME":
+            pass
+
+        elif current_state == "GAMEOVER":
+            pass
+
+        if current_state != previous_state:
+            print("Switching from state "+ previous_state + " to " + current_state)
+            previous_state = current_state
